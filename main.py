3bb071a61e8afcdd3f6754b3886ba8aad63b67d7
Christiaan Janssen <christiaanjanssen@outlook.com>
2025-09-18 09:06:26 +0200

    Print the results to the screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, convert_dict_to_list
 
 
 def get_book_text(path: str) -> str:
@@ -11,9 +11,17 @@ def get_book_text(path: str) -> str:
 
 def main():
     num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    char_counts = get_num_chars(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    print(char_counts)
+    letter_counts = convert_dict_to_list(
+        get_num_chars(get_book_text("books/frankenstein.txt"))
+    )
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for k in letter_counts:
+        print(f"{k['name']}: {k['num']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -7,9 +7,22 @@ def get_num_chars(text: str) -> dict[str, int]:
     for word in text:
         for c in word:
             c = c.lower()
-            if c in counts:
-                counts[c] += 1
-            else:
-                counts[c] = 1
+            if c.isalpha():
+                if c in counts:
+                    counts[c] += 1
+                else:
+                    counts[c] = 1
 
     return counts
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def convert_dict_to_list(char_counts: dict[str, int]) -> list[dict[str, int]]:
+    l = []
+    for k, v in char_counts.items():
+        l.append({"name": k, "num": v})
+    l.sort(reverse=True, key=sort_on)
+    return l
